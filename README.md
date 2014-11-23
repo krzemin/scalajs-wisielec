57c119a29e9a807170045b3a8b80b83ee0809752
Piotr Krzemiński <pio.krzeminski@gmail.com>
2014-11-23 17:56:49 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# Play Framework with Scala.js
+
+This is a simple example application showing how you can integrate a Play project with a Scala.js project.
+
+The application contains three directories:
+* `scalajvm` Play application (server side)
+* `scalajs` Scala.js application (client side)
+* `scala` scala code that you want to share between scalajs and scalajvm (both client and server sides)
+
+## Run the application
+```shell
+$ sbt
+> run
+$ open http://localhost:9000
+```
+
+## IDE integration
+
+### Eclipse
+
+1. `$ sbt eclipse`
+2. Inside Eclipse, `File/Import/General/Existing project...`, choose the root folder to import the projects
+
+### IntelliJ
+
+1. `$ sbt gen-idea`
+2. Inside IntelliJ, `File/Open...`, choose the root folder to import all the projects (do *not* use `Import Project...` or `Import Module...`)
+
+## Notes
+* `fastOptJS` is triggered when compiling (works with `compile`, `~compile`, `run`, `~run`)
+* `fullOptJS` is triggered when calling Play `start`, `stage` or `dist` command
+* The SourceMaps controller returns scala files in dev mode only
---
+++ project/Build.scala
@@ -0,0 +1,113 @@
+import sbt._
+import Keys._
+import play.Play._
+import scala.scalajs.sbtplugin.ScalaJSPlugin._
+import ScalaJSKeys._
+import com.typesafe.sbt.packager.universal.UniversalKeys
+import com.typesafe.sbteclipse.core.EclipsePlugin.EclipseKeys
+
+object ApplicationBuild extends Build with UniversalKeys {
+
+  val scalajsOutputDir = Def.settingKey[File]("directory for javascript files output by scalajs")
+
+  override def rootProject = Some(scalajvm)
+
+  val sharedSrcDir = "scala"
+
+  lazy val scalajvm = Project(
+    id = "scalajvm",
+    base = file("scalajvm")
+  ) enablePlugins (play.PlayScala) settings (scalajvmSettings: _*) aggregate (scalajs)
+
+  lazy val scalajs = Project(
+    id = "scalajs",
+    base = file("scalajs")
+  ) settings (scalajsSettings: _*)
+
+  lazy val sharedScala = Project(
+    id = "sharedScala",
+    base = file(sharedSrcDir)
+  ) settings (sharedScalaSettings: _*)
+
+  lazy val scalajvmSettings =
+    Seq(
+      name := "play-wisielec",
+      version := Versions.app,
+      scalaVersion := Versions.scala,
+      scalajsOutputDir := (classDirectory in Compile).value / "public" / "javascripts",
+      compile in Compile <<= (compile in Compile) dependsOn (fastOptJS in (scalajs, Compile)) dependsOn copySourceMapsTask,
+      dist <<= dist dependsOn (fullOptJS in (scalajs, Compile)),
+      stage <<= stage dependsOn (fullOptJS in (scalajs, Compile)),
+      libraryDependencies ++= Dependencies.scalajvm.value,
+      EclipseKeys.skipParents in ThisBuild := false,
+      commands += preStartCommand
+    ) ++ (
+      // ask scalajs project to put its outputs in scalajsOutputDir
+      Seq(packageLauncher, fastOptJS, fullOptJS) map { packageJSKey =>
+        crossTarget in (scalajs, Compile, packageJSKey) := scalajsOutputDir.value
+      }
+    ) ++ sharedDirectorySettings
+
+  lazy val scalajsSettings =
+    scalaJSSettings ++ Seq(
+      name := "scalajs-wisielec",
+      version := Versions.app,
+      scalaVersion := Versions.scala,
+      persistLauncher := true,
+      persistLauncher in Test := false,
+      relativeSourceMaps := true,
+      libraryDependencies ++= Dependencies.scalajs.value
+    ) ++ sharedDirectorySettings
+
+  lazy val sharedScalaSettings =
+    Seq(
+      name := "shared-wisielec",
+      libraryDependencies ++= Dependencies.shared.value
+    )
+
+  lazy val sharedDirectorySettings = Seq(
+    unmanagedSourceDirectories in Compile += new File((file(".") / sharedSrcDir / "src" / "main" / "scala").getCanonicalPath),
+    unmanagedSourceDirectories in Test += new File((file(".") / sharedSrcDir / "src" / "test" / "scala").getCanonicalPath),
+    unmanagedResourceDirectories in Compile += file(".") / sharedSrcDir / "src" / "main" / "resources",
+    unmanagedResourceDirectories in Test += file(".") / sharedSrcDir / "src" / "test" / "resources"
+  )
+
+  val copySourceMapsTask = Def.task {
+    val scalaFiles = (Seq(sharedScala.base, scalajs.base) ** ("*.scala")).get
+    for (scalaFile <- scalaFiles) {
+      val target = new File((classDirectory in Compile).value, scalaFile.getPath)
+      IO.copyFile(scalaFile, target)
+    }
+  }
+
+  // Use reflection to rename the 'start' command to 'play-start'
+  Option(play.Play.playStartCommand.getClass.getDeclaredField("name")) map { field =>
+    field.setAccessible(true)
+    field.set(playStartCommand, "play-start")
+  }
+
+  // The new 'start' command optimises the JS before calling the Play 'start' renamed 'play-start'
+  val preStartCommand = Command.args("start", "<port>") { (state: State, args: Seq[String]) =>
+    Project.runTask(fullOptJS in (scalajs, Compile), state)
+    state.copy(remainingCommands = ("play-start " + args.mkString(" ")) +: state.remainingCommands)
+  }
+}
+
+object Dependencies {
+  val shared = Def.setting(Seq())
+
+  val scalajvm = Def.setting(shared.value ++ Seq(
+    "org.webjars" % "jquery" % "1.11.1"
+  ))
+
+  val scalajs = Def.setting(shared.value ++ Seq(
+    "org.scala-lang.modules.scalajs" %%% "scalajs-dom" % Versions.scalajsDom,
+    "org.scala-lang.modules.scalajs" %% "scalajs-jasmine-test-framework" % scalaJSVersion % "test"
+  ))
+}
+
+object Versions {
+  val app = "0.1.0-SNAPSHOT"
+  val scala = "2.11.4"
+  val scalajsDom = "0.6"
+}
