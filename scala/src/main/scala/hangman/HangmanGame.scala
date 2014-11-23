3da7fb3dea3c93ef293eb7b1db34c7ff74f14b5e
Piotr Krzemiński <pio.krzeminski@gmail.com>
2014-11-23 19:33:57 +0100

    hangman game implementation

---
+++ scala/src/main/scala/hangman/HangmanGame.scala
@@ -0,0 +1,46 @@
+package hangman
+
+object HangmanGame {
+
+  trait GameState
+  case class InProgress(mistakesLeft: Int) extends GameState
+  case object Lost extends GameState
+  case class Won(steps: Int) extends GameState
+
+  class Game(word: String, maxMistakes: Int = 5) {
+
+    var moves: Vector[Char] = Vector.empty
+    var visible: String = "_" * word.size
+    var mistakes: Int = 0
+
+    def move(letter: Char): Unit = {
+      state match {
+        case InProgress(_) =>
+          moves = moves :+ letter
+          if(word.contains(letter)) {
+            val indicies = word.zipWithIndex.filter(_._1 == letter).map(_._2)
+            visible = visible.zipWithIndex.map {
+              case (let, idx) => if(indicies.contains(idx)) letter else let
+            }.mkString
+          } else {
+            mistakes = mistakes + 1
+          }
+        case _ =>
+      }
+    }
+
+    def state: GameState = {
+      if(mistakes < maxMistakes) {
+        if(visible == word) {
+          Won(moves.size)
+        } else {
+          InProgress(maxMistakes - mistakes)
+        }
+      } else {
+        Lost
+      }
+    }
+  }
+
+
+}
