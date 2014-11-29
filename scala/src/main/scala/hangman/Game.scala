3d5e7e507c3b1307af8bde9a3e68565a64e6a93f
Piotr Krzemiński <pio.krzeminski@gmail.com>
2014-11-29 15:27:26 +0100

    add missing letter in alphabet (Z)

--- scala/src/main/scala/hangman/Game.scala
+++ scala/src/main/scala/hangman/Game.scala
@@ -6,7 +6,7 @@ class Game(word: String, maxMistakes: Int = 5) {
   var moves: Vector[Char] = Vector.empty
   var visible: String = "_" * word.size
   var mistakes: Int = 0
-  val alphabet = "AĄBCĆDEĘFGHIJKLŁMNŃOÓPRSTUWXYŹŻ"
+  val alphabet = "AĄBCĆDEĘFGHIJKLŁMNŃOÓPRSTUWXYZŹŻ"
 
   def move(letter: Char): Boolean = {
     state match {
